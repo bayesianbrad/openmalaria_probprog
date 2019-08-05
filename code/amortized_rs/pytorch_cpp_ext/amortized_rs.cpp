f1640e9ec4a25e3b6901565a24c84bd8cc0af072
Christian Schroeder de Witt <schroederdewitt@gmail.com>
2019-08-06 01:07:40 +0200

    added hogwild mode and pytorch timer

--- code/amortized_rs/pytorch_cpp_ext/amortized_rs.cpp
+++ code/amortized_rs/pytorch_cpp_ext/amortized_rs.cpp
@@ -11,16 +11,20 @@ Scalar R2(Scalar z_2, Scalar z_3);
 
 
 at::Tensor f(void) {
-  Scalar z_1 = at::empty({1}, kDouble).normal_(0.0, 1.0).item();
+  Scalar z_1 = at::empty({1}, kFloat).normal_(0.0, 1.0).item();
   Scalar z_2;
+
+  int ctr = 0;
   do {
     z_2 = R1(z_1, 0);
-  } while (z_2.to<double>() == std::numeric_limits<double>::infinity());
-  Scalar z_3 = at::empty({1}, kDouble).uniform_().item();
+    ctr += 1;
+  } while (z_2.to<float>() == std::numeric_limits<float>::infinity());
+  //std::cout << ctr;
+  Scalar z_3 = at::empty({1}, kFloat).uniform_(0,2).item();
   Scalar z_4 = R2(z_2, z_3);
   //std::cout << z_1 << "|" << z_2 << "|" << z_3 << "|" << z_4;
-  //double data[] = {z_1.to<double>(), z_2.to<double>(), z_3.to<double>(), z_4.to<double>()};
-  auto ret = torch::empty({4}, kDouble);
+  //float data[] = {z_1.to<float>(), z_2.to<float>(), z_3.to<float>(), z_4.to<float>()};
+  auto ret = torch::empty({4}, kFloat);
   ret[0] = z_1;
   ret[1] = z_2;
   ret[2] = z_3;
@@ -29,21 +33,32 @@ at::Tensor f(void) {
 }
 
 Scalar R1(Scalar z_1, unsigned int i) {
-    Scalar temp = at::empty({1}, kDouble).normal_(z_1.to<double>(), 1.0).item();
-    if (temp.to<double>() > 0){
-        return temp;
-    } else if (i == 1000){
-        return CPU(kDouble).scalarTensor(std::numeric_limits<double>::infinity()).item();
-    } else {
-        i += 1;
-        return R1(z_1, i);
+    Scalar temp;
+//    if (temp.to<float>() > 0){
+//        return temp;
+//    } else if (i == 100){
+//        return CPU(kFloat).scalarTensor(std::numeric_limits<float>::infinity()).item();
+//    } else {
+//        i += 1;
+//        return R1(z_1, i);
+//    }
+    long j = 0;
+    while (true){
+        temp = at::empty({1}, kFloat).normal_(z_1.to<float>(), 1.0).item();
+        if (temp.to<float>() > 0){
+            return temp;
+        } else if (j == 10000){
+            return CPU(kFloat).scalarTensor(std::numeric_limits<float>::infinity()).item();
+        } else {
+            j += 1;
+        }
     }
 }
 
 Scalar R2(Scalar z_2, Scalar z_3) {
-    Scalar temp = at::empty({1}, kDouble).normal_(z_3.to<double>(), 1.0).item();
-    while (temp.to<double>()  < z_2.to<double>() ) {
-        temp = torch::randint(1, {1}).normal_(z_3.to<double>(), 1.0).item();
+    Scalar temp = at::empty({1}, kFloat).normal_(z_3.to<float>(), 1.0).item();
+    while (temp.to<float>()  < z_2.to<float>() ) {
+        temp = at::empty({1}, kFloat).normal_(z_3.to<float>(), 1.0).item();
     }
     return temp;
 }
