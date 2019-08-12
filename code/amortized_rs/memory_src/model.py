0d7e584aa9f50f0cac21924c5c9cc9d1ab7556bd
bradley <bradleygramhansen@gmail.com>
2019-08-12 09:19:23 +0100

    updated

--- code/amortized_rs/memory_src/model.py
+++ code/amortized_rs/memory_src/model.py
@@ -8,17 +8,15 @@ class density_estimator(nn.Module):
         self.hidden = nn.Sequential(
             nn.Linear(inputSize, inputSize//2,bias=True),
             nn.ReLU(),
-            # nn.Linear(inputSize // 2, inputSize, bias=True)
             nn.Linear(inputSize//2, inputSize//(inputSize//2),bias=True) # ensures that the output would be 2, assuming inputsize = 2^n
             )
         self.linear_mu = nn.Linear( inputSize//(inputSize//2), outputSize)
         self.linear_std = nn.Linear( inputSize//(inputSize//2), outputSize)
-        # self.linear_mu = nn.Linear(inputSize, outputSize)
-        # self.linear_std = nn.Linear(inputSize, outputSize)
 
     def forward(self, x):
          # x is of shape [batch_size, input_dim]
         hidden = self.hidden(x) # return [batch_size//2, 2]
-        return self.linear_mu(hidden), F.softplus(self.linear_std(hidden))
-
+        mu = self.linear_mu(hidden)
+        sigma = F.softplus(self.linear_std(hidden))
+        return mu, sigma
 
